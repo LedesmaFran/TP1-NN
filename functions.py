2241576e2521aaa90c6ac2148172533bd205099f
LedesmaFran <fledesma@itba.edu.ar>
2022-09-03 22:13:51 -0300

    progress tp1

--- functions.py
+++ functions.py
@@ -2,18 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
-import math
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 from scipy.stats import multivariate_normal
 
 
-def plot_meanVector_covMatrix(class_vector,df,columns,onlyclass=None,plot=True):
+def meanVector_covMatrix(class_vector,df,columns,onlyclass=None,plot=True):
     if onlyclass is not None:
         class_vector=onlyclass
-    rvs = []
+    result = {}
     for class_name in class_vector:
-        rvs_temp = []
+        result_temp = []
         class_data = df[df['Class']==class_name]
         mean_vector = []
         for col in columns:
@@ -21,9 +18,12 @@ def plot_meanVector_covMatrix(class_vector,df,columns,onlyclass=None,plot=True):
                 mean_vector.append(round(mean,4))
 
         covMatrix = pd.DataFrame.cov(class_data)
-        rvs_temp.append(mean_vector)
-        rvs_temp.append(covMatrix)
-        rvs_temp.append(100)
+
+        result_temp.append(mean_vector)
+        result_temp.append(covMatrix)
+
+        result[class_name] = result_temp
+
         if plot:
             print("Name class:", class_name)
             print('u_vector =',mean_vector)
@@ -36,8 +36,41 @@ def plot_meanVector_covMatrix(class_vector,df,columns,onlyclass=None,plot=True):
             cb.ax.tick_params(labelsize=14)
             plt.title('Correlation Matrix: '+class_name, fontsize=16);
             plt.show()
-        rvs.append(rvs_temp)
-    return rvs
+    return result
+
+# def meanVector_covMatrix(class_vector,df,columns,onlyclass=None,plot=True):
+#     if onlyclass is not None:
+#         class_vector=onlyclass
+#     result = []
+#     for class_name in class_vector:
+#         result_temp = []
+#         class_data = df[df['Class']==class_name]
+#         mean_vector = []
+#         for col in columns:
+#                 mean = class_data[col].mean()
+#                 mean_vector.append(round(mean,4))
+
+#         covMatrix = pd.DataFrame.cov(class_data)
+
+#         result_temp.append(mean_vector)
+#         result_temp.append(covMatrix)
+
+#         if plot:
+#             print("Name class:", class_name)
+#             print('u_vector =',mean_vector)
+#             print('cov_matrix:\n',round(covMatrix,4),end='\n\n')
+#             f = plt.figure(figsize=(8, 6))
+#             plt.matshow(df.corr(), fignum=f.number)
+#             plt.xticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=14, rotation=45)
+#             plt.yticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=14)
+#             cb = plt.colorbar()
+#             cb.ax.tick_params(labelsize=14)
+#             plt.title('Correlation Matrix: '+class_name, fontsize=16);
+#             plt.show()
+#         result.append(result_temp)
+#     return result
+
+
 
 def plot_data_as_gaussian(class_vector,df,columns,onlyclass=None):
     if onlyclass is not None:
@@ -55,6 +88,7 @@ def plot_data_as_gaussian(class_vector,df,columns,onlyclass=None):
         plt.title(class_name)
         plt.grid()
         plt.show()
+
 
 
 def plot_data(class_vector,df,columns,onlyclass=None):
@@ -84,15 +118,3 @@ def print_data(class_vector,df,columns,onlyclass=None):
             print("mean:",round(class_data[col].mean(),3), end='\t')
             print("desv std:",round(class_data[col].std(),3))
         print("\n")
-
-
-
-#Funcion cselmo
-def generate_dataset(random_variables):
-    X = np.array([]).reshape(0, len(random_variables[0][0]))
-    y = np.array([]).reshape(0, 1)
-    for i, rv in enumerate(random_variables):
-        X = np.vstack([X, np.random.multivariate_normal(rv[0], rv[1], rv[2])])
-        y = np.vstack([y, np.ones(rv[2]).reshape(rv[2],1)*i]) 
-    y = y.reshape(-1)
-    return X, y
